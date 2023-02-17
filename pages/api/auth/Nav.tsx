66ea65b536c9bb295a50cebe9bed69f1f628ac3a
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-02-17 09:27:40 +0100

    Create Nav.tsx

---
+++ pages/api/auth/Nav.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+
+
+const Nav=()=>{
+    return(
+        <nav>
+            <Link href={"/"}>
+                <h1>Send it.</h1>
+            </Link>
+        </nav>
+    )
+    
+}
+
+export default Nav
+  
