32d8ac335b5afb8831502158ffdfa98a46f1d27f
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-02-22 10:37:43 +0100

    Fetching  all post

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 'use Client'
 
+import axios from 'axios'
 import AddPost from './components/AddPost'
+import { useQuery } from '@tanstack/react-query'
+
+
+//Fetch all post
+
+const allPosts= async()=>{
+  const response=await axios.get('/api/posts/getPosts')
+  return response.data
+}
 
 
 export default function Home() {
+  
+  
   return (
     <main >
-      <h1>Hello Next</h1> 
+      
       <AddPost/>  
     </main>
   )
