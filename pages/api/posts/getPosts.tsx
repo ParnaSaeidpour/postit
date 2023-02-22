bb84da9a55650936992f21d31c6c0925b8a50366
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-02-22 10:22:44 +0100

    Create getPosts

---
+++ pages/api/posts/getPosts.tsx
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from "../../../prisma/client"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if(req.method === "GOST"){
+    
+
+
+      //Fetch all  Post
+      try{
+        const data =await prisma.post.findMany()
+        res.status(200).json(data)
+
+      }catch(err){
+        res.status(403).json({err:'Error fetching posts'})
+        
+      }
+
+    } 
+}
