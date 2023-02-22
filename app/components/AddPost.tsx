9135df81a7aaf88824e0c62fc9e5a98004e132f7
Parna Saeidpour <parnasaeidpour@gmail.com>
2023-02-22 10:09:33 +0100

    Create toast error and success

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -1,18 +1,33 @@
 'use client'
 import { useState } from "react"
 import { useMutation,useQueryClient } from "@tanstack/react-query"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
+import { toast } from "react-hot-toast"
 
 export default function CreatePost(){
     const [title,setTitle]=useState("")
     const [isDisabled, setIsDisabled]=useState(false)
+    
 
     //create a post
 
     const {mutate}=useMutation(
         async (title:string)=>
-         await axios.post("/api/posts/addPosts", {title})
+         await axios.post("/api/posts/addPosts", {title}),
+         {
+            onError:(error)=>{
+                if(error instanceof AxiosError){
+                    toast.error(error?.response?.data.message)
 
+                }
+                setIsDisabled(false)
+            },
+            onSuccess:(data)=>{
+                toast.success("Post has been made 🔥")
+                setTitle("")
+                setIsDisabled(false)
+            },
+         }
     )
 
     const submitPost =async (e:React.FormEvent)=>{
